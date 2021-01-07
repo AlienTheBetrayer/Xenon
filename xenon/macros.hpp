19939644df97a79749a11104f92a181c57d200a3
AlienTheBetrayer <Alien@TheBetrayer.com>
2021-01-07 17:52:20 +0200

    macros.hpp

---
+++ xenon/macros.hpp
@@ -0,0 +1,27 @@
+// macros.hpp
+// 
+// All the needed macros for Xenon.
+
+#ifndef XENON_HG_MACROS
+#define XENON_HG_MACROS
+
+#ifdef _WIN32
+// Windows OS
+#define XENON_M_WIN
+
+#ifdef WIN32_LEAN_AND_MEAN
+#define XENON_M_WLAM
+#endif // WIN32_LEAN_AND_MEAN
+
+// C++ version is bigger or equals to 20
+#if defined(_MSVC_LANG) && _MSVC_LANG > 201703L || __cplusplus >= 201703L
+#define XENON_M_CPP20GRT
+#endif // defined(_MSVC_LANG) && _MSVC_LANG > 201703L || __cplusplus >= 201703L
+
+// C++ version is bigger or equals to 17
+#if defined(_MSVC_LANG) && _MSVC_LANG >= 201703L || __cplusplus >= 201703L
+#define XENON_M_CPP17GRT
+#endif // defined(_MSVC_LANG) && _MSVC_LANG >= 201703L || __cplusplus >= 201703L
+#endif // _WIN32
+
+#endif // XENON_HG_MACROS
