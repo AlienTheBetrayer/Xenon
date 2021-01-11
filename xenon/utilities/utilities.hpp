aaaa59787f07be791aea004e0e68a5b447322b31
AlienTheBetrayer <Alien@TheBetrayer.com>
2021-01-11 18:11:13 +0200

    parts folder

--- xenon/utilities/utilities.hpp
+++ xenon/utilities/utilities.hpp
@@ -6,9 +6,9 @@
 #define XENON_HG_UTILITIES_MODULE
 
 // Including all the parts of this module.
-#include "vector2.hpp"
-#include "vector3.hpp"
-#include "vector4.hpp"
-#include "rect.hpp"
+#include "parts/vector2.hpp"
+#include "parts/vector3.hpp"
+#include "parts/vector4.hpp"
+#include "parts/rect.hpp"
 
 #endif // XENON_HG_UTILIES_MODULE
