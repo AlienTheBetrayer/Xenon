d6e1023ee4067e0b2957761c7ef7e2b3281984aa
AlienTheBetrayer <Alien@TheBetrayer.com>
2021-01-13 17:37:59 +0200

    console module start

--- xenon/console/console.hpp
+++ xenon/console/console.hpp
@@ -12,29 +12,99 @@
 
 // All the libraries
 #ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
+//#define WIN32_LEAN_AND_MEAN
 #endif
 
 #include <Windows.h>
+
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "Kernel32.lib")
 
 // Xenon's Modules
 #include "../utilities/utilities.hpp"
 
 namespace xenon {
     namespace console {
-        // Get functions
-        
-        [[nodiscard]] HANDLE get_handle(const DWORD type = STD_OUTPUT_HANDLE) noexcept;
-        [[nodiscard]] HWND get_window(void) noexcept; 
+        /**
+         * @brief Gets the std handle of the console.
+         * @param  type: The type of the std handle
+         * @retval The handle
+         */
+        [[nodiscard]] HANDLE get_handle(const DWORD type = STD_OUTPUT_HANDLE) noexcept {
+            const HANDLE handle = GetStdHandle(type);
+            return handle;
+        }
 
-        // Get console's properties functions
+        /**
+         * @brief Gets the console's window.
+         * @retval The window
+         */
+        [[nodiscard]] HWND get_window(void) noexcept {
+            const HWND window = GetConsoleWindow();
+            return window;
+        }
 
-        [[nodiscard]] xenon::utilities::Vector2<uint32_t> get_window_size(void) noexcept;
-        [[nodiscard]] xenon::utilities::Vector2<uint32_t> get_buffer_size(void) noexcept;
-        [[nodiscard]] xenon::utilities::Rect<uint32_t> get_window_bounds(void) noexcept;
+        /**
+         * @brief Sets the console's window size
+         * @param  size: New size
+         * @retval None
+         */
+        void set_window_size(const xenon::utilities::Vector2<uint32_t>& size) noexcept {
+            const HWND window = get_window();
+            RECT c_rect;
+            GetWindowRect(window, &c_rect);
+            MoveWindow(window, c_rect.left, c_rect.top, size.x, size.y, TRUE);
+        }
 
-        // Set console's properties functions
-        void set_window_size(const xenon::utilities::Vector2<uint32_t>& size) noexcept;
+        /**
+         * @brief Sets the new buffer size for the console.
+         * @param size: New size of the console's window buffer
+         * @retval None
+         */
+        void set_buffer_size(const xenon::utilities::Vector2<uint32_t>& size) noexcept {
+            COORD buffer = { static_cast<int16_t>(size.x), static_cast<int16_t>(size.y) };
+            SetConsoleScreenBufferSize(get_handle(), buffer);
+        }
+
+        /**
+         * @brief Sets the new window bounds.
+         * @param bounds: New bounds of the console's window
+         * @retval None
+         */
+        void set_window_bounds(const xenon::utilities::Rect<uint32_t>& bounds) noexcept {
+            SetWindowPos(get_window(), nullptr, bounds.left, bounds.top, bounds.right - bounds.left, bounds.bottom - bounds.top, SWP_NOZORDER);
+        }
+
+
+        /**
+         * @brief Gets the console's buffer size. 
+         * @retval Buffer size vector
+         */
+        [[nodiscard]] xenon::utilities::Vector2<uint32_t> get_buffer_size(void) noexcept {
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+            GetConsoleScreenBufferInfo(get_handle(), &csbi);
+            return xenon::utilities::Vector2<uint32_t>{ static_cast<uint32_t>(csbi.dwSize.X), static_cast<uint32_t>(csbi.dwSize.Y) };
+        }
+
+        /**
+         * @brief Gets the console window bounds.
+         * @retval A console's window bounds 
+         */
+        [[nodiscard]] xenon::utilities::Rect<uint32_t> get_window_bounds(void) noexcept {
+            RECT c_rect;
+            GetWindowRect(get_window(), &c_rect);
+            return xenon::utilities::Rect<uint32_t>{ static_cast<uint32_t>(c_rect.left), static_cast<uint32_t>(c_rect.right), static_cast<uint32_t>(c_rect.top), static_cast<uint32_t>(c_rect.bottom) };
+        }
+
+        /**
+         * @brief Gets the console's window size.
+         * @retval Window size vector
+         */
+        [[nodiscard]] xenon::utilities::Vector2<uint32_t> get_window_size(void) noexcept {
+            xenon::utilities::Rect<uint32_t> c_rect = get_window_bounds();
+            return xenon::utilities::Vector2<uint32_t>{ c_rect.right - c_rect.left, c_rect.bottom - c_rect.top };
+        }
+
     } // namespace console
 } // namespace xenon
 
