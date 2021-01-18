2eb84c9eedb88f12388b1698e2eca383afbcc464
AlienTheBetrayer <Alien@TheBetrayer.com>
2021-01-18 16:14:18 +0200

    time module

--- xenon/time/time.hpp
+++ xenon/time/time.hpp
@@ -15,7 +15,10 @@
 #include "../async/async.hpp"
 
 namespace {
+    // -- Helper functions. Should not be used by the user. -- \\ 
+
     template<typename F, typename... Args>
+        requires xenon::concepts::callable<F, Args...>
     inline void XENON_HF_set_timeout(F&& func, const uint32_t delay_ms, const bool async, Args&&... args) noexcept {
         const auto function = [&func, delay_ms]() {
             std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
@@ -27,7 +30,31 @@ namespace {
 
 namespace xenon {
     namespace time {
-        
+        /**
+         * @brief Waits the timeout and then runs the function(asynchronously). 
+         * @note   
+         * @param  func: The function
+         * @param  timeout: After how many milliseconds to run the function
+         * @param  args: Args
+         * @retval None
+         */
+        template<typename F, typename... Args>
+        inline void set_async_timeout(F&& func, const uint32_t timeout, Args&&... args) noexcept {
+            XENON_HF_set_timeout(func, timeout, true, std::forward<Args>(args)...);
+        }
+
+        /**
+         * @brief Waits the timeout and then runs the function(synchronously). 
+         * @note   
+         * @param  func: The function
+         * @param  timeout: After how many milliseconds to run the function
+         * @param  args: Args
+         * @retval None
+         */
+        template<typename F, typename... Args>
+        inline void set_sync_timeout(F&& func, const uint32_t timeout, Args&&... args) noexcept {
+            XENON_HF_set_timeout(func, timeout, false, std::forward<Args>(args)...);
+        }
     } // namespace time
 } // namespace xenon
 
